13ef046955196308bb958452f4d26b8c701e2155
Keenan Humm <keenan.humm@gmail.com>
2020-12-25 09:57:42 -0600

    adds login page and functionality

--- src/generated/graphql.tsx
+++ src/generated/graphql.tsx
@@ -1,5 +1,5 @@
-import gql from 'graphql-tag';
-import * as Urql from 'urql';
+import gql from "graphql-tag";
+import * as Urql from "urql";
 export type Maybe<T> = T | null;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
@@ -15,56 +15,56 @@ export type Scalars = {
 };
 
 export type Query = {
-  __typename?: 'Query';
-  posts: Array<Post>;
+  __typename?: "Query";
+  posts: Post[];
   post?: Maybe<Post>;
   me?: Maybe<User>;
 };
 
 
 export type QueryPostArgs = {
-  id: Scalars['Int'];
+  id: Scalars["Int"];
 };
 
 export type Post = {
-  __typename?: 'Post';
-  id: Scalars['Float'];
-  createdAt: Scalars['String'];
-  updatedAt: Scalars['String'];
-  title: Scalars['String'];
+  __typename?: "Post";
+  id: Scalars["Float"];
+  createdAt: Scalars["String"];
+  updatedAt: Scalars["String"];
+  title: Scalars["String"];
 };
 
 export type User = {
-  __typename?: 'User';
-  id: Scalars['Float'];
-  createdAt: Scalars['String'];
-  updatedAt: Scalars['String'];
-  username: Scalars['String'];
+  __typename?: "User";
+  id: Scalars["Float"];
+  createdAt: Scalars["String"];
+  updatedAt: Scalars["String"];
+  username: Scalars["String"];
 };
 
 export type Mutation = {
-  __typename?: 'Mutation';
+  __typename?: "Mutation";
   createPost: Post;
   updatePost?: Maybe<Post>;
-  deletePost: Scalars['Boolean'];
+  deletePost: Scalars["Boolean"];
   register: UserResponse;
   login: UserResponse;
 };
 
 
 export type MutationCreatePostArgs = {
-  title: Scalars['String'];
+  title: Scalars["String"];
 };
 
 
 export type MutationUpdatePostArgs = {
-  title?: Maybe<Scalars['String']>;
-  id: Scalars['Float'];
+  title?: Maybe<Scalars["String"]>;
+  id: Scalars["Float"];
 };
 
 
 export type MutationDeletePostArgs = {
-  id: Scalars['Float'];
+  id: Scalars["Float"];
 };
 
 
@@ -78,47 +78,89 @@ export type MutationLoginArgs = {
 };
 
 export type UserResponse = {
-  __typename?: 'UserResponse';
-  errors?: Maybe<Array<FieldError>>;
+  __typename?: "UserResponse";
+  errors?: Maybe<FieldError[]>;
   user?: Maybe<User>;
 };
 
 export type FieldError = {
-  __typename?: 'FieldError';
-  field: Scalars['String'];
-  message: Scalars['String'];
+  __typename?: "FieldError";
+  field: Scalars["String"];
+  message: Scalars["String"];
 };
 
 export type UserCredentials = {
-  username: Scalars['String'];
-  password: Scalars['String'];
+  username: Scalars["String"];
+  password: Scalars["String"];
 };
 
+export type LoginUserMutationVariables = Exact<{
+  username: Scalars["String"];
+  password: Scalars["String"];
+}>;
+
+
+export type LoginUserMutation = (
+  { __typename?: "Mutation" }
+  & {
+    login: (
+      { __typename?: "UserResponse" }
+      & {
+        errors?: Maybe<(
+          { __typename?: "FieldError" }
+          & Pick<FieldError, "field" | "message">
+        )[]>, user?: Maybe<(
+          { __typename?: "User" }
+          & Pick<User, "id" | "username">
+        )>,
+      }
+    ),
+  }
+);
+
 export type RegisterMutationVariables = Exact<{
-  username: Scalars['String'];
-  password: Scalars['String'];
+  username: Scalars["String"];
+  password: Scalars["String"];
 }>;
 
 
 export type RegisterMutation = (
-  { __typename?: 'Mutation' }
+  { __typename?: "Mutation" }
   & {
     register: (
-      { __typename?: 'UserResponse' }
+      { __typename?: "UserResponse" }
       & {
-        errors?: Maybe<Array<(
-          { __typename?: 'FieldError' }
-          & Pick<FieldError, 'field' | 'message'>
-        )>>, user?: Maybe<(
-          { __typename?: 'User' }
-          & Pick<User, 'id' | 'username' | 'createdAt' | 'updatedAt'>
-        )>
+        errors?: Maybe<(
+          { __typename?: "FieldError" }
+          & Pick<FieldError, "field" | "message">
+        )[]>, user?: Maybe<(
+          { __typename?: "User" }
+          & Pick<User, "id" | "username">
+        )>,
       }
-    )
+    ),
   }
 );
 
 
+export const LoginUserDocument = gql`
+    mutation LoginUser($username: String!, $password: String!) {
+  login(credentials: {username: $username, password: $password}) {
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      username
+    }
+  }
+}
+    `;
+
+export function useLoginUserMutation() {
+  return Urql.useMutation<LoginUserMutation, LoginUserMutationVariables>(LoginUserDocument);
+}
 export const RegisterDocument = gql`
     mutation Register($username: String!, $password: String!) {
   register(credentials: {username: $username, password: $password}) {
@@ -129,8 +171,6 @@ export const RegisterDocument = gql`
     user {
       id
       username
-      createdAt
-      updatedAt
     }
   }
 }
@@ -138,4 +178,4 @@ export const RegisterDocument = gql`
 
 export function useRegisterMutation() {
   return Urql.useMutation<RegisterMutation, RegisterMutationVariables>(RegisterDocument);
-};
+}
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,27 +3,27 @@ import { Formik, Form } from "formik";
 import { Button, Heading } from "@chakra-ui/react";
 import Wrapper from "../components/Wrapper";
 import InputField from "../components/InputField";
-import { useRegisterMutation } from "../generated/graphql";
+import { useLoginUserMutation } from "../generated/graphql";
 import toErrorMap from "../utils/toErrorMap";
 import { useRouter } from "next/router";
 
-const Register = () => {
-  const [{ }, registerUser] = useRegisterMutation();
+const Login = () => {
+  const [{ }, loginUser] = useLoginUserMutation();
   const router = useRouter();
 
   return (
     <Wrapper size="sm">
-      <Heading>Register</Heading>
+      <Heading>Log in</Heading>
       <Formik
         initialValues={{
           username: "",
           password: "",
         }}
         onSubmit={async (values, { setErrors }) => {
-          const response = await registerUser(values);
-          if (response.data?.register.errors) {
-            setErrors(toErrorMap(response.data.register.errors));
-          } else if (response.data?.register.user) {
+          const response = await loginUser(values);
+          if (response.data?.login.errors) {
+            setErrors(toErrorMap(response.data.login.errors));
+          } else if (response.data?.login.user) {
             router.push("/");
           }
         }}
@@ -49,7 +49,7 @@ const Register = () => {
               aria-label="Register"
               size="lg"
             >
-              register
+              log in
             </Button>
           </Form>
         )}
@@ -58,4 +58,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Login;
